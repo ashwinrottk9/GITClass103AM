0474683da02cd064f3b6a1d2479c9140b4dabbc4
ashwinrottk9 <ashwinrottk9@gmail.com>
2025-02-22 12:06:16 +0530

    Added codes from Ashwin branch

---
+++ src/test/java/com/sample/ClassB.java
@@ -0,0 +1,20 @@
+package com.sample;
+
+public class ClassB {
+
+	private void webApplication() {
+		System.out.println("Selenium");
+
+	}
+
+	private void mobileApplication() {
+		System.out.println("Appium");
+
+	}
+
+	private void apiTesting() {
+		System.out.println("Postman");
+
+	}
+
+}
